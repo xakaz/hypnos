d12240792a812968adc7b48cf447b1f5cddb8673
xakaz <loic.hernandez@sfr.fr>
2022-04-09 10:49:54 +0200

    import react-router-dom & 1st Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Accueil</h1>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
