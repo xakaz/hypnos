aa427f1e4614a00de8d20021b51950a61d2949ca
xakaz <loic.hernandez@sfr.fr>
2022-04-09 11:35:31 +0200

    Navbar & Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<h1>Accueil</h1>}/>
+          <Route path="/hotels" element={<h1>Hotels</h1>}/>
+          <Route path="/bien-etre" element={<h1>Bien-être</h1>}/>
+          <Route path="/restaurants" element={<h1>Restaurants</h1>}/>
+          <Route path="/evenements" element={<h1>Evènements</h1>}/>
+          <Route path="/contact" element={<h1>Contact</h1>}/>
+          <Route path="*" element={<h1>Erreur 404</h1>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Navbar.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import Logo from '../assets/Navbar/logoHotel.jpg'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light px-4">
+
+        {/* LOGO */}
+        <div>
+          <img src={Logo} alt="hypnosLogo" style={{height:"50px"}} className='img-fluid'/>
+        </div>
+
+        {/* ONGLETS PAGES */}
+        <div>
+          <div className="row">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+              <div>
+                
+                {/* BOUTON RESPONSIVE*/ }
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav mb-2 mb-lg-0 ">
+
+                    {/* ACCUEIL */ }
+                    <li className="nav-item">
+                      <NavLink to='/' className='nav-link ms-3'>Accueil</NavLink>
+                    </li>
+
+                    {/* HOTELS */ }
+                    <li className="nav-item">
+                      <NavLink to='/hotels' className='nav-link ms-3'>Hotels</NavLink>
+                    </li>
+                    
+
+                    {/* RESTOS */ }
+                    <li className="nav-item">
+                      <NavLink to='/restaurants' className='nav-link ms-3'>Restauration</NavLink>
+                    </li>
+
+                    {/* BIEN-ETRE */ }
+                    <li className="nav-item">
+                      <NavLink to='/bien-etre' className='nav-link ms-3'>Bien-être</NavLink>
+                    </li>
+                    
+                    {/* EVENEMENTS */ }
+                    <li className="nav-item">
+                      <NavLink to='/evenements' className='nav-link ms-3'>Evènements</NavLink>
+                    </li>
+                    
+                    {/* CONTACT */ }
+                    <li className="nav-item">
+                      <NavLink to='/contact' className='nav-link ms-3'>Contact</NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+
+        {/* BOUTONS INSCRIPTION - CONNEXION - DECONNEXION */}
+        <div>
+          <button className="btn btn-primary ms-2">
+            Mon compte
+          </button>
+          <button className="btn btn-primary ms-2">
+            Inscription
+          </button>
+          <button className="btn btn-primary ms-2">
+            Connexion
+          </button>
+          <button className="btn btn-danger ms-2">
+            Deconnexion
+          </button>
+        </div>
+      </nav>
+    </>
+  )
+}
