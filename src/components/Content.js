9d531b196bb437ae0b98ad2c26420f3e728f38a8
xakaz <loic.hernandez@sfr.fr>
2022-04-09 16:13:24 +0200

    Creation Content.js

---
+++ src/components/Content.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Content = (props) => {
+  return (
+    <div className='my-3 container'>
+      <div className="row text-white">
+        <div className="col-6 d-flex justify-content-end ">
+          <img  src={props.vignette} 
+                alt={props.vignette_nom} 
+                style={{width:"500px"}} 
+                className="rounded img-fluid"/>
+        </div>
+        <div className="col-6 d-flex align-items-center">
+          <div className="row">
+            <h5>{props.title}</h5>
+            <hr></hr>
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Content
