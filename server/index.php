2d32c9acf8cc7e84e804f9a9321fe341c7d74ecb
xakaz <loic.hernandez@sfr.fr>
2022-04-10 19:44:35 +0200

    routes dans index.php

--- server/index.php
+++ server/index.php
@@ -17,7 +17,30 @@ try{
     if(empty($_GET['url'])){
          throw new Exception("La page n'existe pas") ;
      } else {
-         echo "La page demandée est : ".$_GET['url'];
+        $url = explode("/", filter_var($_GET['url'], FILTER_SANITIZE_URL));
+        if( empty($url[0]) || empty($url[1]) ) throw new Exception("La page n'existe pas");
+        switch($url[0]){
+            case "front" :
+                switch($url[1]){
+                    case "hotels" : echo "la page des hotels";
+                    break;
+                    case "suites" : echo "la page des suites";
+                    break;
+                    case "restaurants" : echo "la page des restaurants";
+                    break;
+                    case "bien-etre" : echo "la page bien-être";
+                    break;
+                    case "evenements" : echo "la page des évènements";
+                    break;
+                    case "contact" : echo "la page des contacts";
+                    break;
+                    default : throw new Exception("La page n'existe pas") ;
+                }
+            break;
+            case "back" : echo "backend de mon application";
+            break;
+            default : throw new Exception("La page n'existe pas") ;
+        }
      }
  } catch (Exception $e) {
      $msg = $e->getMessage();
